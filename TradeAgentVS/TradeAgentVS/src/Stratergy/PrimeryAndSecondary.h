74e608bf3024b3882f58708f93372c63191f047b
barney-li <barney.li@outlook.com>
2014-08-11 15:27:56 +0000

    1. 回测中带有成交判断功能的中间版本。
    git-svn-id: file:///d/clouddrive/SyncFolder/svnRepository/trunk@160 3ddc6574-91de-f842-91ed-6e7e04a71137

--- TradeAgentVS/TradeAgentVS/src/Stratergy/PrimeryAndSecondary.h
+++ TradeAgentVS/TradeAgentVS/src/Stratergy/PrimeryAndSecondary.h
@@ -178,7 +178,7 @@ public:
 		{
 			lWinRate = 100*mWin/(mWin+mLose);
 		}
-		tempStream<<stgArg.bollPeriod<<"	"<<stgArg.outterBollAmp<<"	"<<(int)(stgArg.bollAmpLimit/stgArg.minMove)<<"	"<<stgArg.winBollAmp<<"	"<<(int)(stgArg.stopLossPrice/stgArg.minMove)<<"	"<<(int)(stgArg.winBollAmpAdjust)<<"	"<<(int)(stgArg.durationStep)<<"	"<<(int)(stgArg.stopWinPoint/stgArg.minMove)<<"	"<<stgArg.primaryInst<<"-"<<stgArg.secondaryInst<<"	"<<lWinRate<<"	"<<mTotalProfit<<"	"<<mWin<<"	"<<mLose;
+		tempStream<<stgArg.bollPeriod<<"	"<<stgArg.outterBollAmp<<"	"<<(int)(stgArg.bollAmpLimit/stgArg.minMove)<<"	"<<stgArg.winBollAmp<<"	"<<(int)(stgArg.stopLossPrice/stgArg.minMove)<<"	"<<(int)(stgArg.stopWinPoint/stgArg.minMove)<<"	"<<stgArg.winBollAmpAdjust<<"	"<<(int)(stgArg.durationStep)<<"	"<<stgArg.primaryInst<<"-"<<stgArg.secondaryInst<<"	"<<lWinRate<<"	"<<mTotalProfit<<"	"<<mWin<<"	"<<mLose;
 		mTestStatistics.LogThisNoTimeStamp(tempStream.str().c_str());
 		cout<<tempStream.str()<<endl;
 #endif
@@ -598,11 +598,97 @@ public:
 	/************************************************************************/
 	void AsyncEventPoster(void)
 	{
-		while(!mEventQueue.empty())
+		int lPostTime=0;
+		const BasicMarketData &lPrim = primDataBuf[primBufIndex];
+		const BasicMarketData &lScnd = scndDataBuf[scndBufIndex];
+		switch(mStateMachine.GetState())
 		{
-			SetEvent(mEventQueue.front());
-			mEventQueue.pop();
+		case IDLE_STATE:
+			break;
+		case OPENING_SCND_STATE:
+			break;
+		case CHECKING_SCND_STATE:
+			SetEvent(SCND_OPENED);
+			break;
+		case OPENING_PRIM_STATE:
+			break;
+		case PENDING_STATE:
+			break;
+		case CLOSING_BOTH_STATE:
+			break;
+		case CANCELLING_SCND_STATE:
+			break;
+		case CLOSING_SCND_STATE:
+			break;
+		case CANCELLING_PRIM_STATE:
+			break;
+		case WAITING_SCND_CLOSE_STATE:
+			break;
+		case WAITING_PRIM_CLOSE_STATE:
+			break;
+		default:
+			break;
 		}
+	}
+	/************************************************************************/
+	// 回测中，OPENING_SCND_STATE状态下的异步事件产生函数
+	/************************************************************************/
+	void OpeningScndStateAsyncEventGenerator()
+	{
+		const BasicMarketData &lPrim = primDataBuf[primBufIndex];
+		const BasicMarketData &lScnd = scndDataBuf[scndBufIndex];
+		if(OPEN_COND1 == mOpenCond)
+		{
+			if(mScndEnterPrice>=lScnd.askPrice)
+			{
+				mScndEnterPrice = lScnd.askPrice;
+				tempStream.clear();
+				tempStream.str("");
+				tempStream<<"[EVENT]: SCND_OPENED (from async poster) at: "<<mScndEnterPrice;
+				SetEvent(SCND_OPENED);
+			}// 买开，当我的出价大于等于盘口卖价时成交
+		}// 买次主力
+		else
+		{
+			if(mScndEnterPrice<=lScnd.bidPrice)
+			{
+				mScndEnterPrice = lScnd.bidPrice;
+				tempStream.clear();
+				tempStream.str("");
+				tempStream<<"[EVENT]: SCND_OPENED (from async poster) at: "<<mScndEnterPrice;
+				SetEvent(SCND_OPENED);
+			}// 卖开，当我的出价小于等于盘口买价时成交
+		}// 卖次主力
+	}
+	/************************************************************************/
+	// 回测中，OPENING_PRIM_STATE状态下的异步事件产生函数
+	/************************************************************************/
+	void OpeningPrimStateAsyncEventGenerator()
+	{
+		const BasicMarketData &lPrim = primDataBuf[primBufIndex];
+		const BasicMarketData &lScnd = scndDataBuf[scndBufIndex];
+		if(OPEN_COND2 == mOpenCond)
+		{
+			if(mPrimEnterPrice>=lPrim.askPrice)
+			{
+				mPrimEnterPrice = lPrim.askPrice;
+				tempStream.clear();
+				tempStream.str("");
+				tempStream<<"[EVENT]: PRIM_OPENED (from async poster) at: "<<mPrimEnterPrice;
+				SetEvent(PRIM_OPENED);
+			}// 买开，当我的出价大于等于盘口卖价时成交
+		}// 买主力
+		else
+		{
+			if(mPrimEnterPrice<=lPrim.bidPrice)
+			{
+				mPrimEnterPrice = lPrim.bidPrice;
+				tempStream.clear();
+				tempStream.str("");
+				tempStream<<"[EVENT]: PRIM_OPENED (from async poster) at: "<<mPrimEnterPrice;
+				SetEvent(PRIM_OPENED);
+			}// 卖开，当我的出价小于等于盘口买价时成交
+		}// 卖主力
 	}
 #endif
 public:
